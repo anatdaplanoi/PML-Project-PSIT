f908129549edd0e32188c01d1a4e703a8a1c60d9
Aalums <59070194@it.kmitl.ac.th>
2016-10-30 14:28:25 +0700

    open file

---
+++ project.py
@@ -0,0 +1,7 @@
+"""This program is shows graph about A​nalysis of teenage pregnancy"""
+def openfile():
+    """This function is open file"""
+    read = open("E:/แอล/KMITL/PSIT/Project/data.txt", "r")
+    for i in read:
+        print(i)
+main()
