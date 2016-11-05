3336b907bde9715a375a59cd48c094257b5f9f9a
Aalums <59070194@it.kmitl.ac.th>
2016-11-05 15:16:34 +0700

    docstring and function

--- project.py
+++ project.py
@@ -1,21 +1,25 @@
+"""This program is shows graph about A​nalysis of teenage pregnancy"""
 import pylab
-read = open("C:/Users/Planoi/Desktop/project/project/data.txt", "r")
-da_lis = []
-da_year = []
-da_amount = []
-for i in read:
-    da_lis.append(i.split())
-for i in range(len(da_lis)):
-    da_year.append(int(da_lis[i][0]))
-    da_amount.append(float(da_lis[i][4]))
-pylab.plot(da_year, da_amount,'-bo',linewidth=1)
-labelProperty = dict(fontweight='bold',fontsize='12')
-pylab.xlabel('Years', labelProperty)
-pylab.ylabel('Pregnancy', labelProperty)
-legends = ('Average total time')
-pylab.legend(legends,
- loc='upper left', shadow=True, fancybox=True)
-pylab.xlim(2500,2560)
-pylab.xticks(da_year)
-pylab.grid(True)
-pylab.show()
+def main():
+    """This function is open file"""
+    read = open("C:/Users/Planoi/Desktop/project/project/data.txt", "r")
+    da_lis = []
+    da_year = []
+    da_amount = []
+    for i in read:
+        da_lis.append(i.split())
+    for i in range(len(da_lis)):
+        da_year.append(int(da_lis[i][0]))
+        da_amount.append(float(da_lis[i][4]))
+    pylab.plot(da_year, da_amount,'-bo',linewidth=1)
+    labelProperty = dict(fontweight='bold',fontsize='12')
+    pylab.xlabel('Years', labelProperty)
+    pylab.ylabel('Pregnancy', labelProperty)
+    legends = ('Average total time')
+    pylab.legend(legends,
+     loc='upper left', shadow=True, fancybox=True)
+    pylab.xlim(2500,2560)
+    pylab.xticks(da_year)
+    pylab.grid(True)
+    pylab.show()
+main()
