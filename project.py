c07c6e3d86348a0a6cf091a92b6a2bbf282eb192
Aalums <59070194@it.kmitl.ac.th>
2016-11-05 15:44:30 +0700

    change year line 21

--- project.py
+++ project.py
@@ -2,7 +2,7 @@
 import pylab
 def main():
     """This function is open file"""
-    read = open("C:/Users/Planoi/Desktop/project/project/data.txt", "r")
+    read = open("C:/Users/Aal/Desktop/project/data.txt", "r")
     da_lis = []
     da_year = []
     da_amount = []
@@ -11,14 +11,14 @@ def main():
     for i in range(len(da_lis)):
         da_year.append(int(da_lis[i][0]))
         da_amount.append(float(da_lis[i][4]))
-    pylab.plot(da_year, da_amount,'-bo',linewidth=1)
+    pylab.plot(da_year, da_amount,'-bo',linewidth=2)
     labelProperty = dict(fontweight='bold',fontsize='12')
     pylab.xlabel('Years', labelProperty)
     pylab.ylabel('Pregnancy', labelProperty)
     legends = ('Average total time')
     pylab.legend(legends,
      loc='upper left', shadow=True, fancybox=True)
-    pylab.xlim(2500,2560)
+    pylab.xlim(2533,2555)
     pylab.xticks(da_year)
     pylab.grid(True)
     pylab.show()
