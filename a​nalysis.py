149c356620a7217baebda8264a5a0ae684655227
Aalums <59070194@it.kmitl.ac.th>
2016-11-07 16:49:16 +0700

    Rename project to analysis

---
+++ a​nalysis.py
@@ -0,0 +1,28 @@
+"""This program is shows graph about A​nalysis of teenage pregnancy"""
+import pylab
+def main():
+    """This function is open file"""
+    read = open("C:/Users/Aal/Desktop/project/data.txt", "r")
+    da_lis = []
+    da_year = []
+    da_amount = []
+    count = 0
+    for i in read:
+        if count != 0:
+            da_lis.append(i.split())
+        count = 1
+    for i in range(len(da_lis)):
+        da_year.append(int(da_lis[i][0]))
+        da_amount.append(float(da_lis[i][6]))
+    pylab.plot(da_year, da_amount,'-bo',linewidth=2)
+    labelProperty = dict(fontweight='bold',fontsize='12')
+    pylab.xlabel('Years', labelProperty)
+    pylab.ylabel('Pregnancy', labelProperty)
+    legends = ('Average total time',)
+    pylab.legend(legends,
+     loc='upper left', shadow=True, fancybox=True)
+    pylab.xlim(2533,2555)
+    pylab.xticks(da_year)
+    pylab.grid(True)
+    pylab.show()
+main()
