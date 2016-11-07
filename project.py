626afc89362f802a049131a92c1029a69fbb632d
Aalums <59070194@it.kmitl.ac.th>
2016-11-07 16:28:23 +0700

    change data

--- project.py
+++ project.py
@@ -6,11 +6,14 @@ def main():
     da_lis = []
     da_year = []
     da_amount = []
+    count = 0
     for i in read:
-        da_lis.append(i.split())
+        if count != 0:
+            da_lis.append(i.split())
+        count = 1
     for i in range(len(da_lis)):
         da_year.append(int(da_lis[i][0]))
-        da_amount.append(float(da_lis[i][4]))
+        da_amount.append(float(da_lis[i][6]))
     pylab.plot(da_year, da_amount,'-bo',linewidth=2)
     labelProperty = dict(fontweight='bold',fontsize='12')
     pylab.xlabel('Years', labelProperty)
