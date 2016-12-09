d7ac1c10f92500ce4bb90e57f7d617c7a301823e
Anatda Planoi <59070184@it.kmitl.ac.th>
2016-12-09 14:43:29 +0700

    change variable

--- percent how to abort.py
+++ percent how to abort.py
@@ -4,10 +4,10 @@ import matplotlib.pyplot as plt
 
 def show_graph():
     """The graph shows the method used to terminate a pregnancy."""
-    read = open("C:/Users/Planoi/Desktop/project/project/PML-Project-PSIT/วิธีการยุติการตั้งครรภ์ที่ใช้.txt", "r")
+    read = open("C:/Users/Planoi/project/project/PML-Project-PSIT/วิธีการยุติการตั้งครรภ์ที่ใช้.txt", "r")
     ind = np.arange(7)  # the x locations for the groups
     fig = plt.figure()
-    ax = fig.add_subplot(111)
+    ax_size = fig.add_subplot(111)
     da_lis = []
     year_2542 = []
     year_2554 = []
@@ -21,24 +21,24 @@ def show_graph():
         year_2542.append(float(da_lis[0][i]))
         year_2554.append(float(da_lis[1][i]))
         year_2555.append(float(da_lis[2][i]))
-    rects1 = ax.bar(ind, year_2542, 0.24, color='r')
-    rects2 = ax.bar(ind + 0.24, year_2554, 0.24, color='g')
-    rects3 = ax.bar(ind + 0.24*2, year_2555, 0.24, color='b')
-    ax.set_ylabel('abortion methods(%)')
-    ax.set_xticks(ind + 0.36)
-    ax.set_xticklabels(('vacuum aspiration', 'Pill', 'Pessary', 'Curettage',\
+    rects1 = ax_size.bar(ind, year_2542, 0.24, color='r')
+    rects2 = ax_size.bar(ind + 0.24, year_2554, 0.24, color='g')
+    rects3 = ax_size.bar(ind + 0.24*2, year_2555, 0.24, color='b')
+    ax_size.set_ylabel('abortion methods(%)')
+    ax_size.set_xticks(ind + 0.36)
+    ax_size.set_xticklabels(('vacuum aspiration', 'Pill', 'Pessary', 'Curettage',\
         'solid into the vagina', 'liquid into the vagina', 'Squeeze the belly'))
-    ax.legend((rects1[0], rects2[0], rects3[0]), ('2542', '2554', '2555'))
-    autolabel(rects1, ax)
-    autolabel(rects2, ax)
-    autolabel(rects3, ax)
+    ax_size.legend((rects1[0], rects2[0], rects3[0]), ('2542', '2554', '2555'))
+    autolabel(rects1, ax_size)
+    autolabel(rects2, ax_size)
+    autolabel(rects3, ax_size)
     plt.show()
 
-def autolabel(rects, ax):
+def autolabel(rects, ax_size):
     """Assign a name to the bar chart in the middle."""
     for i in rects:
         high = i.get_height()
-        ax.text(i.get_x() + i.get_width()/2, 1.00*high, '%.1f'%float(high),\
+        ax_size.text(i.get_x() + i.get_width()/2, 1.00*high, '%.1f'%float(high),\
             ha='center', va='bottom')
 
 show_graph()
--- percent_abort.py
+++ percent_abort.py
@@ -4,10 +4,10 @@ import matplotlib.pyplot as plt
 
 def graph_abort():
     """The graph shows the provider to terminate the pregnancy."""
-    read = open("C:/Users/Planoi/Desktop/project/project/PML-Project-PSIT/ผู้ให้บริการยุติการตั้งครรภ์.txt", "r")
+    read = open("C:/Users/Planoi/project/project/PML-Project-PSIT/ผู้ให้บริการยุติการตั้งครรภ์.txt", "r")
     ind = np.arange(4)  # the x locations for the groups
     fig = plt.figure()
-    ax = fig.add_subplot(111)
+    ax_size = fig.add_subplot(111)
     da_lis = []
     year_2542 = []
     year_2554 = []
@@ -21,24 +21,24 @@ def graph_abort():
         year_2542.append(float(da_lis[0][i]))
         year_2554.append(float(da_lis[1][i]))
         year_2555.append(float(da_lis[2][i]))
-    rects1 = ax.bar(ind, year_2542, 0.24, color='r')
-    rects2 = ax.bar(ind + 0.24, year_2554, 0.24, color='g')
-    rects3 = ax.bar(ind + 0.24*2, year_2555, 0.24, color='b')
-    ax.set_ylabel('abort(%)')
-    ax.set_xticks(ind + 0.36)
-    ax.set_xticklabels(('abort by public health', 'abort by not public health',\
+    rects1 = ax_size.bar(ind, year_2542, 0.24, color='r')
+    rects2 = ax_size.bar(ind + 0.24, year_2554, 0.24, color='g')
+    rects3 = ax_size.bar(ind + 0.24*2, year_2555, 0.24, color='b')
+    ax_size.set_ylabel('abort(%)')
+    ax_size.set_xticks(ind + 0.36)
+    ax_size.set_xticklabels(('abort by public health', 'abort by not public health',\
         'unknow', 'self abortion'))
-    ax.legend((rects1[0], rects2[0], rects3[0]), ('2542', '2554', '2555'))
-    autolabel(rects1, ax)
-    autolabel(rects2, ax)
-    autolabel(rects3, ax)
+    ax_size.legend((rects1[0], rects2[0], rects3[0]), ('2542', '2554', '2555'))
+    autolabel(rects1, ax_size)
+    autolabel(rects2, ax_size)
+    autolabel(rects3, ax_size)
     plt.show()
 
-def autolabel(rects, ax):
+def autolabel(rects, ax_size):
     """Assign a name to the bar chart in the middle."""
     for i in rects:
         high = i.get_height()
-        ax.text(i.get_x() + i.get_width()/2, 1.00*high, '%.1f'%float(high), ha='center',\
+        ax_size.text(i.get_x() + i.get_width()/2, 1.00*high, '%.1f'%float(high), ha='center',\
             va='bottom')
 
 graph_abort()
